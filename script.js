be31385d488e32ae6183b398bff743b0b6621f85
henriquep2025 <henrique.p.oliveira6@aluno.senai.br>
2025-06-17 16:47:55 -0300

    calculadora final

--- script.js
+++ script.js
@@ -5,24 +5,36 @@ const resultado = document.getElementById ("resultado")
 
 imcForm.addEventListener('submit', function (event) {
     event.preventDefault();
-    calcularIMC(peso, altura);
+    calcularIMC(peso.value, altura.value);
 })
 
 function calcularIMC(p, a){
-    const conta = p/ (a*a);
+    const imc = p/ (a*a);
+    const conta = imc.toFixed(2);
     let classificacao = ""
-    if (condicao){
+    if (conta<18.5){
+        classificacao = "Abaixo do Peso"
+    }else if (conta <= 24.9){
+        classificacao = "Normal"
+    } else if (conta <= 29.9){
+        classificacao = "Sobrepeso"
+    } else if (conta <= 34.9){
+        classificacao = "Obesidade I"
+    } else if (conta <= 39.9){
+        classificacao = "Obesidade II"
+    } else {
+        classificacao = "Obesidade III"
+    };
 
-    }else {
-        
-    }
+    console.log(`Seu peso é ${p}, sua altura é ${a} e seu IMC é ${conta}, seu nível é ${classificacao}`)
+    resultado.textContent = `Seu peso é ${p}, sua altura é ${a} e seu IMC é ${conta}, seu nível é ${classificacao}`
 
     //IF e ELSE para definir as categorias do IMC
-    // menor 18,5 - baixo peso
-    // de 18,5 a 24,9 - normal
-    // 25 a 29,9 - sobrepeso
-    // 30 a 34,9 - obesidade I
-    // 35 a 39,9 - obesidade II
-    // 40+ - obesidade III
+    // menor 18,5 - baixo peso (conta<18,5)
+    // de 18,5 a 24,9 - normal ()
+    // 25 a 29,9 - sobrepeso ()
+    // 30 a 34,9 - obesidade I ()
+    // 35 a 39,9 - obesidade II ()
+    // 40+ - obesidade III ()
 
 }
